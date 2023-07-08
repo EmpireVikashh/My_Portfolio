53da8407bd8e093a67c897d2e60121d3bd96496e
Vikash sharma <116377338+EmpireVikashh@users.noreply.github.com>
2023-07-08 23:33:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// console.log("vikash");
+let btn = document.getElementsByClassName('btn');
+btn[1].addEventListener('click',()=>{
+    // window.location.href = "https://www.ilovepdf.com/compress_pdf";
+    window.open("https://github.com/EmpireVikashh?tab=repositories","_blank")
+})
+// console.log(btn);
+
+btn[0].addEventListener('click', function() {
+  window.open('https://drive.google.com/file/d/1j1MO0L6rz0d0wvQHxwkUWRywVTtGVxIN/view?usp=drive_link','_blank');
+});
+
+
+
